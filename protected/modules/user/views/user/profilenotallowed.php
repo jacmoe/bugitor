8f0a0bc6d7381c170224b7504ceb67325a4a456b
jacmoe <jacmoe@localhost>
2010-10-10 09:33:23 +0200

    yii-user-management module is crap - removing..

--- protected/modules/user/views/user/profilenotallowed.php
+++
@@ -1,6 +0,0 @@
-<?php $this->menu = array(YumMenuItemHelper::backToProfile());?>
-
-<div class="hint">
-	<p> <?php echo Yii::t('UserModule.user',
-	  'You are not allowed to view this profile.'); ?> </p>
-</div>
