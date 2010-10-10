045671f63306cf9af144e5cfe6a4ccf6a46b33b1
Jacob Moen <jacmoe.dk@gmail.com>
2010-10-10 10:24:30 +0200

    UserIdentity overwritten - Eol issues

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,6 +7,10 @@
  */
 class UserIdentity extends CUserIdentity
 {
+	private $_id;
+	const ERROR_EMAIL_INVALID=3;
+	const ERROR_STATUS_NOTACTIV=4;
+	const ERROR_STATUS_BAN=5;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -17,17 +21,36 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
+		if (strpos($this->username,"@")) {
+			$user=User::model()->notsafe()->findByAttributes(array('email'=>$this->username));
+		} else {
+			$user=User::model()->notsafe()->findByAttributes(array('username'=>$this->username));
+		}
+		if($user===null)
+			if (strpos($this->username,"@")) {
+				$this->errorCode=self::ERROR_EMAIL_INVALID;
+			} else {
+				$this->errorCode=self::ERROR_USERNAME_INVALID;
+			}
+		else if(Yii::app()->controller->module->encrypting($this->password)!==$user->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
+		else if($user->status==0&&Yii::app()->controller->module->loginNotActiv==false)
+			$this->errorCode=self::ERROR_STATUS_NOTACTIV;
+		else if($user->status==-1)
+			$this->errorCode=self::ERROR_STATUS_BAN;
+		else {
+			$this->_id=$user->id;
+			$this->username=$user->username;
 			$this->errorCode=self::ERROR_NONE;
+		}
 		return !$this->errorCode;
+	}
+    
+    /**
+    * @return integer the ID of the user record
+    */
+	public function getId()
+	{
+		return $this->_id;
 	}
 }
