6cf68ead597d45820fb413cea5e86f0f0481d844
Jacob Moen <jacmoe.dk@gmail.com>
2010-10-13 03:22:21 +0200

    Modifying controllers to be managed by Rights module

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -1,6 +1,6 @@
 <?php
 
-class IssueController extends Controller
+class IssueController extends RightsBaseController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -14,7 +14,7 @@ class IssueController extends Controller
 	public function filters()
 	{
 		return array(
-			'accessControl', // perform access control for CRUD operations
+			'rights', // perform access control for CRUD operations
 		);
 	}
 
--- protected/controllers/IssuePriorityController.php
+++ protected/controllers/IssuePriorityController.php
@@ -1,6 +1,6 @@
 <?php
 
-class IssuePriorityController extends Controller
+class IssuePriorityController extends RightsBaseController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -14,7 +14,7 @@ class IssuePriorityController extends Controller
 	public function filters()
 	{
 		return array(
-			'accessControl', // perform access control for CRUD operations
+			'rights', // perform access control for CRUD operations
 		);
 	}
 
