361c8cc4cfa37accdd55fe5b64a241b83e4f4bdd
Jacob Moen <jacmoe.dk@gmail.com>
2010-10-30 03:01:05 +0200

    More fixture work

---
+++ protected/tests/fixtures_not_used/bug_issue_priority.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'issue_priority_1' => array(
+        'name' => 'Low',
+    ),
+    'issue_priority_2' => array(
+        'name' => 'Normal',
+    ),
+    'issue_priority_3' => array(
+        'name' => 'High',
+    ),
+);
---
+++ protected/tests/fixtures_not_used/bug_tracker.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'tracker_1' => array(
+        'name' => 'Bug',
+    ),
+    'tracker_2' => array(
+        'name' => 'Feature',
+    ),
+);
