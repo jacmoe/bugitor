d28efc800bb9a9b2e5947969cbc9298949bcafc6
jacmoe <jacmoe@localhost>
2010-10-30 06:14:09 +0200

    Forgot URL rules in production config

--- protected/config/main.php
+++ protected/config/main.php
@@ -64,6 +64,8 @@ return array(
             'urlFormat' => 'path',
             'showScriptName' => false,
             'rules' => array(
+                '/projects/' => 'project/index',
+                '/projects/<name>' => 'project/view',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
