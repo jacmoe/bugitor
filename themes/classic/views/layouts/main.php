718d30474ca61a6c1ae8e42a6978c13e3a3836ef
Jacob Moen <jacmoe.dk@gmail.com>
2010-10-31 01:07:03 +0200

    New theme in the works.. :)

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -5,24 +5,37 @@
         <meta name="language" content="en" />
 
         <!-- blueprint CSS framework -->
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-        <!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/screen.css" media="screen, projection" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/print.css" media="print" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/ie.css" media="screen, projection" />
 
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/plugins/fancy-type/screen.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/form.css" />
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     </head>
-
     <body>
-
-        <div class="container" id="page">
-
-            <div id="header">
-                <div id="logo"><?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/bugitor_64.png') ?><?php echo CHtml::encode(Yii::app()->name); ?><div align="right"><?php if (!Yii::app()->user->isGuest): ?><?php
+    <div class=container id="page">
+      <div id="topnav" class="span-16">Home Projects
+      </div>
+      <div id="usernav" class="span-8 last">
+        <span class="right">Login Register
+        </span>
+      </div>
+      <hr/>
+      <div id="header" class="span-24">
+        <div id="logo" class="span-2">
+          <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/bugitor_64.png') ?>
+        </div>
+        <div id="header" class="span-14 alt">
+            <div>
+            <h1 class="alt"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+            </div>
+        </div>
+        <div id="gravatar" class="span-8 last">
+          <div class="right">
+            <?php if (!Yii::app()->user->isGuest): ?>
+                <?php
             $this->widget('application.extensions.VGGravatarWidget',
                     array(
                         'email' => Yii::app()->getModule('user')->user()->email, // email to display the gravatar belonging to it
@@ -35,15 +48,16 @@
                         'htmlOptions' => array('alt' => 'Gravatar Icon'), // Html options that will be appended to the image tag
             ));
 ?>
-<?php endif; ?>
-                    </div></div>
-            </div><!-- header -->
-
-            <div id="mainmenu">
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <div id="mainmenu" class="span-24">
                 <?php
                 $this->widget('zii.widgets.CMenu', array(
                     'items' => array(
-                        array('label' => 'Home', 'url' => array('/site/index')),
+                        array('label' => 'Home', 'url' => array('/projects')),
                         array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
                         array('label' => 'Contact', 'url' => array('/site/contact')),
                         array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), 'visible' => Yii::app()->user->isGuest),
@@ -53,19 +67,14 @@
                         array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
                     ),
                 )); ?>
-                </div><!-- mainmenu -->
-            <?php
-                $this->widget('zii.widgets.CBreadcrumbs', array(
-                    'links' => $this->breadcrumbs,
-                )); ?><!-- breadcrumbs -->
-                <?php echo $content; ?>
-
-            <div id="footer">
-		Copyright &copy; 2010 by Ogitor Team.<br/>
-		All Rights Reserved.<br/>
-<?php echo Yii::powered(); ?>
-            </div><!-- footer -->
-
-        </div><!-- page -->
+      </div>
+      <hr/>
+        <?php echo $content; ?>
+      <hr/>
+      <div class="span-24 alt"><div align="center" class="quiet">
+        Copyright &copy; 2010 by Ogitor Team.<br/>
+        <?php echo Yii::powered(); ?></div>
+      </div>
+    </div>
     </body>
 </html>
