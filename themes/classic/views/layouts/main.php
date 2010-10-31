4269857351bedef3d0fb4d7a51beca923b972bab
jacmoe <jacmoe@localhost>
2010-10-31 05:26:33 +0100

    Links - getting there: context sensitive menus

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -7,7 +7,7 @@
         <!-- blueprint CSS framework -->
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/screen.css" media="screen, projection" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/print.css" media="print" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/ie.css" media="screen, projection" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/ie.css" media="screen, projection" />
 
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/plugins/fancy-type/screen.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/main.css" />
@@ -15,85 +15,90 @@
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     </head>
     <body>
-    <div class=container id="page">
-      <div id="mainmenu" class="span-16">
+        <div class=container id="page">
+            <div id="mainmenu" class="span-16">
                 <?php
                 $this->widget('zii.widgets.CMenu', array(
                     'items' => array(
                         array('label' => 'Home', 'url' => array('/site/index')),
-                        array('label' => 'Projects', 'url' => array('/projects')),
+                        array('label' => 'Projects', 'url' => array('/projects/')),
                     ),
                 )); ?>
-      </div>
-      <div id="mainmenu" class="span-8 last">
-        <span class="right">
+            </div>
+            <div id="mainmenu" class="span-8 last">
+                <span class="right">
+                    <?php
+                    $this->widget('zii.widgets.CMenu', array(
+                        'items' => array(
+                            array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), 'visible' => Yii::app()->user->isGuest),
+                            array('url' => Yii::app()->getModule('user')->registrationUrl, 'label' => Yii::app()->getModule('user')->t("Register"), 'visible' => Yii::app()->user->isGuest),
+                            array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
+                            array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
+                        ),
+                    )); ?>
+                </span>
+            </div>
+            <hr/>
+            <div id="header" class="span-24">
+                <div id="logo" class="span-2">
+                    <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/bugitor_64.png') ?>
+                </div>
+                <div id="header" class="span-14 alt">
+                    <div>
+                        <h1 class="alt"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+                    </div>
+                </div>
+                <div id="gravatar" class="span-8 last">
+                    <div class="right">
+                        <?php if (!Yii::app()->user->isGuest): ?>
+                        <?php
+                            $this->widget('application.extensions.VGGravatarWidget',
+                                    array(
+                                        'email' => Yii::app()->getModule('user')->user()->email, // email to display the gravatar belonging to it
+                                        'hashed' => false, // if the email provided above is already md5 hashed then set this property to true, defaults to false
+                                        'default' => 'http://www.mysite.com/default_gravatar_image.jpg', // if an email is not associated with a gravatar this image will be displayed,
+                                        // by default this is omitted so the Blue Gravatar icon will be displayed you can also set this to
+                                        // "identicon" "monsterid" and "wavatar" which are default gravatar icons
+                                        'size' => 50, // the gravatar icon size in px defaults to 40
+                                        'rating' => 'PG', // the Gravatar ratings, Can be G, PG, R, X, Defaults to G
+                                        'htmlOptions' => array('alt' => 'Gravatar Icon'), // Html options that will be appended to the image tag
+                            ));
+                        ?>
+                        <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div id="mainmenu" class="span-24">
+                <?php if ((Yii::app()->controller->id === 'project') && (isset($_GET['name']))) : ?>
                 <?php
-                $this->widget('zii.widgets.CMenu', array(
-                    'items' => array(
-                        array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), 'visible' => Yii::app()->user->isGuest),
-                        array('url' => Yii::app()->getModule('user')->registrationUrl, 'label' => Yii::app()->getModule('user')->t("Register"), 'visible' => Yii::app()->user->isGuest),
-                        array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
-                        array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
-                    ),
-                )); ?>
-        </span>
-      </div>
-      <hr/>
-      <div id="header" class="span-24">
-        <div id="logo" class="span-2">
-          <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/bugitor_64.png') ?>
-        </div>
-        <div id="header" class="span-14 alt">
-            <div>
-            <h1 class="alt"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+                                $this->widget('zii.widgets.CMenu', array(
+                                    'items' => array(
+                                        array('label' => 'Overview', 'url' => array('/projects/' . $_GET['name'])),
+                                        array('label' => 'Activity', 'url' => array('/projects/' . $_GET['name'] . '/activity')),
+                                        array('label' => 'Roadmap', 'url' => array('/projects/' . $_GET['name'] . '/roadmap')),
+                                        array('label' => 'Issues', 'url' => array('/projects/' . $_GET['name'] . '/issues')),
+                                        array('label' => 'New Issue', 'url' => array('/projects/' . $_GET['name'] . '/newissue'), 'visible' => !Yii::app()->user->isGuest),
+                                        array('label' => 'Code', 'url' => array('/projects/' . $_GET['name'] . '/code')),
+                                        array('label' => 'Settings', 'url' => array('/projects/' . $_GET['name'] . '/settings'), 'visible' => !Yii::app()->user->isGuest),
+                                    ),
+                                )); ?>
+                <?php else : ?>
+                <?php
+                                    $this->widget('zii.widgets.CMenu', array(
+                                        'items' => array(
+                                            array('label' => 'Welcome', 'url' => array('/site/index')),
+                                            array('label' => 'Projects', 'url' => array('/projects/')),
+                                        ),
+                                    )); ?>
+                <?php endif; ?>
+                                </div>
+                                <hr/>
+            <?php echo $content; ?>
+                                    <hr/>
+                                    <div class="span-24 alt"><div align="center" class="quiet">
+                                            Copyright &copy; 2010 by Ogitor Team.<br/>
+                    <?php echo Yii::powered(); ?></div>
             </div>
         </div>
-        <div id="gravatar" class="span-8 last">
-          <div class="right">
-            <?php if (!Yii::app()->user->isGuest): ?>
-                <?php
-            $this->widget('application.extensions.VGGravatarWidget',
-                    array(
-                        'email' => Yii::app()->getModule('user')->user()->email, // email to display the gravatar belonging to it
-                        'hashed' => false, // if the email provided above is already md5 hashed then set this property to true, defaults to false
-                        'default' => 'http://www.mysite.com/default_gravatar_image.jpg', // if an email is not associated with a gravatar this image will be displayed,
-                        // by default this is omitted so the Blue Gravatar icon will be displayed you can also set this to
-                        // "identicon" "monsterid" and "wavatar" which are default gravatar icons
-                        'size' => 50, // the gravatar icon size in px defaults to 40
-                        'rating' => 'PG', // the Gravatar ratings, Can be G, PG, R, X, Defaults to G
-                        'htmlOptions' => array('alt' => 'Gravatar Icon'), // Html options that will be appended to the image tag
-            ));
-?>
-            <?php endif; ?>
-          </div>
-        </div>
-      </div>
-      <hr/>
-      <div id="mainmenu" class="span-24">
-                <?php if(Yii::app()->controller->id !== 'project') : ?>
-                    <?php
-                    $this->widget('zii.widgets.CMenu', array(
-                        'items' => array(
-                            array('label' => 'Welcome', 'url' => array('/site/index')),
-                            array('label' => 'Projects', 'url' => array('/projects')),
-                        ),
-                    )); ?>
-                <?php elseif(Yii::app()->controller->id === 'project') : ?>
-                    <?php
-                    $this->widget('zii.widgets.CMenu', array(
-                        'items' => array(
-                            array('label' => 'Rights', 'url' => array('/rights'), 'visible' => Yii::app()->user->checkAccess(Rights::module()->superuserName)),
-                        ),
-                    )); ?>
-                <?php endif; ?>
-      </div>
-      <hr/>
-        <?php echo $content; ?>
-      <hr/>
-      <div class="span-24 alt"><div align="center" class="quiet">
-        Copyright &copy; 2010 by Ogitor Team.<br/>
-        <?php echo Yii::powered(); ?></div>
-      </div>
-    </div>
     </body>
 </html>
