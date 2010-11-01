7333b5f68df6c2e901fdcf7ad14ba8957a28cd31
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-01 12:16:08 +0100

    Not showing project switcher if it's out of context

--- themes/classic/views/layouts/column2.php
+++ themes/classic/views/layouts/column2.php
@@ -1,27 +1,29 @@
 <?php $this->beginContent('//layouts/main'); ?>
-	<div class="span-19">
-		<div id="content">
-			<?php echo $content; ?>
-		</div><!-- content -->
-	</div>
-	<div class="span-5 last">
-		<div id="sidebar">
-		<?php
-                        $this->widget('DropDownRedirect', array(
-                            'data' => array('<Switch Project>' => '<Switch Project>' ,'Bugitor' => 'Bugitor', 'MyProject' => 'MyProject'), // data od my dropdownlist
-                            'url' => $this->createUrl($this->route, array_merge($_GET, array('name' => '__value__'))),//'/projects/'.'__value__'), // the url (__value__ will be replaced by the selected value)
-                            'select' => '<Switch Project>', //the preselected value
-                            'htmlOptions'=>array('class'=>'operations')
-                            ));
-			$this->beginWidget('zii.widgets.CPortlet', array(
-				'title'=>'Operations',
-			));
-			$this->widget('zii.widgets.CMenu', array(
-				'items'=>$this->menu,
-				'htmlOptions'=>array('class'=>'operations'),
-			));
-			$this->endWidget();
-		?>
-		</div><!-- sidebar -->
-	</div>
+<div class="span-19">
+    <div id="content">
+        <?php echo $content; ?>
+    </div><!-- content -->
+</div>
+<div class="span-5 last">
+    <div id="sidebar">
+        <?php
+        if (((Yii::app()->controller->id === 'project')||(Yii::app()->controller->id === 'issue')) && (isset($_GET['name']))) {
+            $this->widget('DropDownRedirect', array(
+                'data' => array('<Switch Project>' => '<Switch Project>', 'Bugitor' => 'Bugitor', 'MyProject' => 'MyProject'), // data od my dropdownlist
+                'url' => $this->createUrl($this->route, array_merge($_GET, array('name' => '__value__'))), //'/projects/'.'__value__'), // the url (__value__ will be replaced by the selected value)
+                'select' => '<Switch Project>', //the preselected value
+                'htmlOptions' => array('class' => 'operations')
+            ));
+        }
+        $this->beginWidget('zii.widgets.CPortlet', array(
+            'title' => 'Operations',
+        ));
+        $this->widget('zii.widgets.CMenu', array(
+            'items' => $this->menu,
+            'htmlOptions' => array('class' => 'operations'),
+        ));
+        $this->endWidget();
+        ?>
+    </div><!-- sidebar -->
+</div>
 <?php $this->endContent(); ?>
