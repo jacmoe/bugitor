0c381fda5b07d14340a42bbdd0dbbba8eff14c1f
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-02 14:36:15 +0100

    Issues now has comments

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -1,187 +1,188 @@
 <?php
 
-class IssueController extends RightsBaseController
-{
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+class IssueController extends RightsBaseController {
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'rights', // perform access control for CRUD operations
-		);
-	}
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
 
-	public function allowedActions()
-	{
-		return 'index, view';
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'rights', // perform access control for CRUD operations
+        );
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id, $name='')
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    public function allowedActions() {
+        return 'index, view';
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new Issue;
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        $issue = $this->loadModel($id);
+        $comment = $this->createComment($issue);
+        $this->render('view', array(
+            'model' => $issue,
+            'comment' => $comment,
+        ));
+    }
 
-                $project_name = '';
-                if(isset($_GET['name']))
-                    $project_name = $_GET['name'] . ' - ';
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['Issue']))
-		{
-			$model->attributes=$_POST['Issue'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id, 'name' => $model->project->name));
-		}
-
-		$this->render('create',array(
-			'model'=>$model,
-                        'project_name' => $project_name,
-		));
-	}
-
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
-
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['Issue']))
-		{
-			$model->attributes=$_POST['Issue'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id, 'name' => $model->project->name));
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'index' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		if(Yii::app()->request->isPostRequest)
-		{
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
-
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
-
-        public function getProjects() {
-            $Criteria = new CDbCriteria();
-            $Criteria->select = "name";
-            $results = Project::model()->findAll($Criteria);
-            $project_list = array();
-            foreach($results as $result) {
-                $project_list[$result->name] = $result->name;
+    protected function createComment($issue) {
+        $comment = new Comment;
+        if (isset($_POST['Comment'])) {
+            $comment->attributes = $_POST['Comment'];
+            if ($issue->addComment($comment)) {
+                Yii::app()->user->setFlash('commentSubmitted', "Your comment has been added.");
+                $this->refresh();
             }
-            return $project_list;
+        }
+        return $comment;
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        $model = new Issue;
+
+        $project_name = '';
+        if (isset($_GET['name']))
+            $project_name = $_GET['name'] . ' - ';
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
+
+        if (isset($_POST['Issue'])) {
+            $model->attributes = $_POST['Issue'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id, 'name' => $model->project->name));
         }
 
-        /**
-	 * Lists all models.
-	 */
-	public function actionIndex($name = '')
-	{
-            if($name !== '')
-            {
-                $criteria=new CDbCriteria;
-                $criteria->compare('project_id',$this->getProject($name),true);
-                $dataProvider=new CActiveDataProvider('Issue', array('criteria' =>$criteria));
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-                        'project_name' => $name . ' - ',
-		));
-            } else {
-                $dataProvider=new CActiveDataProvider('Issue');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-                        'project_name' => '',
-		));
-            }
-	}
+        $this->render('create', array(
+            'model' => $model,
+            'project_name' => $project_name,
+        ));
+    }
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new Issue('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Issue']))
-			$model->attributes=$_GET['Issue'];
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        $model = $this->loadModel($id);
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer the ID of the model to be loaded
-	 */
-	public function loadModel($id)
-	{
-		$model=Issue::model()->findByPk((int)$id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
-
-        private function getProject($name)
-        {
-            $project = Project::model()->findByAttributes(array('name' => $name));
-            return $project->id;
+        if (isset($_POST['Issue'])) {
+            $model->attributes = $_POST['Issue'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id, 'name' => $model->project->name));
         }
-/**
-	 * Performs the AJAX validation.
-	 * @param CModel the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='issue-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $this->loadModel($id)->delete();
+
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax']))
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+        }
+        else
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+    }
+
+    public function getProjects() {
+        $Criteria = new CDbCriteria();
+        $Criteria->select = "name";
+        $results = Project::model()->findAll($Criteria);
+        $project_list = array();
+        foreach ($results as $result) {
+            $project_list[$result->name] = $result->name;
+        }
+        return $project_list;
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex($name = '') {
+        if ($name !== '') {
+            $criteria = new CDbCriteria;
+            $criteria->compare('project_id', $this->getProject($name), true);
+            $dataProvider = new CActiveDataProvider('Issue', array('criteria' => $criteria));
+            $this->render('index', array(
+                'dataProvider' => $dataProvider,
+                'project_name' => $name . ' - ',
+            ));
+        } else {
+            $dataProvider = new CActiveDataProvider('Issue');
+            $this->render('index', array(
+                'dataProvider' => $dataProvider,
+                'project_name' => '',
+            ));
+        }
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new Issue('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['Issue']))
+            $model->attributes = $_GET['Issue'];
+
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer the ID of the model to be loaded
+     */
+    public function loadModel($id) {
+        $model = Issue::model()->findByPk((int) $id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
+
+    private function getProject($name) {
+        $project = Project::model()->findByAttributes(array('name' => $name));
+        return $project->id;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'issue-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
 }
--- protected/models/Comment.php
+++ protected/models/Comment.php
@@ -16,92 +16,107 @@
  * @property Users $createUser
  * @property Issue $issue
  */
-class Comment extends CActiveRecord
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return Comment the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class Comment extends CActiveRecord {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return '{{comment}}';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @return Comment the static model class
+     */
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('content', 'required'),
-			array('issue_id, create_user_id, update_user_id', 'numerical', 'integerOnly'=>true),
-			array('created, modified', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, content, issue_id, created, create_user_id, modified, update_user_id', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return '{{comment}}';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'createUser' => array(self::BELONGS_TO, 'Users', 'create_user_id'),
-			'issue' => array(self::BELONGS_TO, 'Issue', 'issue_id'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('content', 'required'),
+            array('issue_id, create_user_id, update_user_id', 'numerical', 'integerOnly' => true),
+            array('created, modified', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, content, issue_id, created, create_user_id, modified, update_user_id', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'content' => 'Content',
-			'issue_id' => 'Issue',
-			'created' => 'Created',
-			'create_user_id' => 'Create User',
-			'modified' => 'Modified',
-			'update_user_id' => 'Update User',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'author' => array(self::BELONGS_TO, 'User', 'create_user_id'),
+            'issue' => array(self::BELONGS_TO, 'Issue', 'issue_id'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'content' => 'Content',
+            'issue_id' => 'Issue',
+            'created' => 'Created',
+            'create_user_id' => 'Author',
+            'modified' => 'Modified',
+            'update_user_id' => 'Update User',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('content',$this->content,true);
-		$criteria->compare('issue_id',$this->issue_id);
-		$criteria->compare('created',$this->created,true);
-		$criteria->compare('create_user_id',$this->create_user_id);
-		$criteria->compare('modified',$this->modified,true);
-		$criteria->compare('update_user_id',$this->update_user_id);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider(get_class($this), array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id);
+        $criteria->compare('content', $this->content, true);
+        $criteria->compare('issue_id', $this->issue_id);
+        $criteria->compare('created', $this->created, true);
+        $criteria->compare('create_user_id', $this->create_user_id);
+        $criteria->compare('modified', $this->modified, true);
+        $criteria->compare('update_user_id', $this->update_user_id);
+
+        return new CActiveDataProvider(get_class($this), array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Prepares create_time, create_user_id, update_time and update_user_id attributes before performing validation.
+     */
+    protected function beforeValidate() {
+        //parent::beforeValidate();
+
+        if ($this->isNewRecord) {
+            // set the create date, last updated date and the user doing the creating
+            $this->created = $this->modified = new CDbExpression('NOW()');
+            $this->create_user_id = $this->update_user_id = Yii::app()->user->id;
+        } else {
+            //not a new record, so just set the last updated time and last updated user id
+            $this->modified = new CDbExpression('NOW()');
+            $this->update_user_id = Yii::app()->user->id;
+        }
+
+        return parent::beforeValidate();
+        //return true;
+    }
+
 }
