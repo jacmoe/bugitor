56389e8e47ecfe461ae5e821d8d2d083ba5ebe21
jacmoe <jacmoe@localhost>
2010-11-03 10:25:39 +0100

    Console entry script

---
+++ console.php
@@ -0,0 +1,20 @@
+<?php
+
+$hostname = $_SERVER['SERVER_NAME'];
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+
+$config=dirname(__FILE__).'/protected/config/console.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+$app = Yii::createConsoleApplication($config);
+
+Yii::app()->config->set('SiteName', 'Bugitor');
+Yii::app()->config->set('HostName', $hostname);
+
+$app->run();
