48a8acd71d2723b033af7a2957d0fd1d8c79dfbe
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-05 10:51:40 -0700

    timekeepr component test

--- protected/components/TimeZoneKeeper.php
+++ protected/components/TimeZoneKeeper.php
@@ -14,14 +14,14 @@ class TimeZoneKeeper extends CComponent{
         }
 
         public function serverToUser($timestamp){
-                $this->userTimezone = new DateTimeZone("EUROPE"/*Yii::app()->user->tz*/);
+                $this->userTimezone = new DateTimeZone('Europe/copenhagen'/*Yii::app()->user->tz*/);
                 $serverDateTime = new DateTime("@".$timestamp);
                 $offset = $this->userTimezone->getOffset($serverDateTime);
                 return ($serverDateTime->format('U') + $offset);
         }
 
         public function userToServer($timestamp){
-                $this->userTimezone = new DateTimeZone("EUROPE"/*Yii::app()->user->tz*/);
+                $this->userTimezone = new DateTimeZone('Europe/copenhagen'/*Yii::app()->user->tz*/);
                 $userDateTime = new DateTime("@".$timestamp);
                 $offset = $this->userTimezone->getOffset($userDateTime);
                 return ($userDateTime->format('U') - $offset);
