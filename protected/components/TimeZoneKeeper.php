fd054dc36d3f146f35b16dac05ff90de0a7b1454
jacmoe <jacmoe@localhost>
2010-11-05 18:39:13 +0100

    Typo in TimeZoneKeeper fixed!

--- protected/components/TimeZoneKeeper.php
+++ protected/components/TimeZoneKeeper.php
@@ -10,18 +10,18 @@ class TimeZoneKeeper extends CComponent{
                 //settiamo il server di default a UTC per rendere l'implementazione  indipendente
                 //dal settaggio del server.
                 Yii::app()->setTimeZone("UTC");
-                $this->serverTimeZone = new DateTimeZone("UTC"/*Yii::app()->getTimeZone()*/);
+                $this->serverTimezone = new DateTimeZone(Yii::app()->getTimeZone());
         }
 
         public function serverToUser($timestamp){
-                $this->userTimeZone = new DateTimeZone('EUROPE'/*Yii::app()->user->tz*/);
+                $this->userTimezone = new DateTimeZone('EUROPE'/*Yii::app()->user->tz*/);
                 $serverDateTime = new DateTime("@".$timestamp);
                 $offset = $this->userTimezone->getOffset($serverDateTime);
                 return ($serverDateTime->format('U') + $offset);
         }
 
         public function userToServer($timestamp){
-                $this->userTimeZone = new DateTimeZone('EUROPE'/*Yii::app()->user->tz*/);
+                $this->userTimezone = new DateTimeZone('EUROPE'/*Yii::app()->user->tz*/);
                 $userDateTime = new DateTime("@".$timestamp);
                 $offset = $this->userTimezone->getOffset($userDateTime);
                 return ($userDateTime->format('U') - $offset);
