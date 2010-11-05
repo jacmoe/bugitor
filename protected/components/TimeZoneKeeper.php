8938fed0b7eacbe06255e422af777bc54d62b648
jacmoe <jacmoe@localhost>
2010-11-05 18:14:06 +0100

    TimeZoneKeeper component to manage server and user timezones

---
+++ protected/components/TimeZoneKeeper.php
@@ -0,0 +1,30 @@
+<?php
+
+class TimeZoneKeeper extends CComponent{
+
+        public $userTimezone;
+        public $serverTimezone;
+
+
+        public function init(){
+                //settiamo il server di default a UTC per rendere l'implementazione  indipendente
+                //dal settaggio del server.
+                Yii::app()->setTimeZone("UTC");
+                $this->serverTimeZone = new DateTimeZone(Yii::app()->getTimeZone());
+        }
+
+        public function serverToUser($timestamp){
+                $this->userTimeZone = new DateTimeZone(Yii::app()->user->tz);
+                $serverDateTime = new DateTime("@".$timestamp);
+                $offset = $this->userTimezone->getOffset($serverDateTime);
+                return ($serverDateTime->format('U') + $offset);
+        }
+
+        public function userToServer($timestamp){
+                $this->userTimeZone = new DateTimeZone(Yii::app()->user->tz);
+                $userDateTime = new DateTime("@".$timestamp);
+                $offset = $this->userTimezone->getOffset($userDateTime);
+                return ($userDateTime->format('U') - $offset);
+        }
+
+}
