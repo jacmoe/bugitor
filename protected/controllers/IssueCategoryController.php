38de347899ed96166901ca484d375f41057b3b12
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-06 12:58:00 +0100

    Timezone is not working

--- protected/controllers/IssueCategoryController.php
+++ protected/controllers/IssueCategoryController.php
@@ -1,6 +1,6 @@
 <?php
 
-class IssueCategoryController extends RightsBaseController
+class IssueCategoryController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
--- protected/controllers/TrackerController.php
+++ protected/controllers/TrackerController.php
@@ -1,6 +1,6 @@
 <?php
 
-class TrackerController extends RightsBaseController
+class TrackerController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
