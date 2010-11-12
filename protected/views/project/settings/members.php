a645089c255d3b491f2e0cdd1a6c1b5ea0dd5b6c
jacmoe <jacmoe@localhost>
2010-11-12 13:32:55 +0100

    Project settings pages: pretty tables - needs actions.

--- protected/views/project/settings/members.php
+++ protected/views/project/settings/members.php
@@ -2,9 +2,38 @@
 $this->pageTitle = $name . ' - Settings - Members';
 ?>
 <h3>Members</h3>
-<?php
-foreach($model as $member) {
-    echo $member->username . '<br/>';
-}
-?>
-<br/>
+<table class="list" width="60%">
+  <thead width="60">
+  <tr>
+      <td colspan="3">
+      Project Members
+      </td>
+  </tr>
+
+  <tr>
+    <th width="20">Username</th>
+    <th width="20">Role</th>
+    <th width="20">Actions</th>
+  </tr>
+  </thead>
+  <tbody>
+<?php foreach($model as $n=>$member): ?>
+  <tr class="<?php echo $n%2?'even':'odd';?>">
+    <td width="20"><?php echo CHtml::encode($member->username); ?></td>
+    <td width="20">
+    <?php $assignments = Rights::getAssignedRoles($member->id); ?>
+    <?php foreach($assignments as $assignment) {
+        echo $assignment->name . ' . ';
+        } ?>
+    </td>
+    <td width="20">
+      <?php echo CHtml::link('Update',array('update','id'=>$member->id)); ?>
+      <?php echo CHtml::linkButton('Delete',array(
+      	  'submit'=>'',
+      	  'params'=>array('command'=>'delete','id'=>$member->id),
+      	  'confirm'=>"Are you sure to delete #{$member->id}?")); ?>
+	</td>
+  </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
--- protected/views/project/settings/repository.php
+++ protected/views/project/settings/repository.php
@@ -2,9 +2,37 @@
 $this->pageTitle = $name . ' - Settings - Repositories';
 ?>
 <h3>Repositories</h3>
-<?php
-foreach($model as $repository) {
-    echo $repository->name . '<br/>';
-}
-?>
-<br/>
+<table class="list" width="60%">
+  <thead width="60">
+  <tr>
+      <td colspan="5">
+      Project Repositories
+      </td>
+  </tr>
+
+  <tr>
+    <th width="20">Name</th>
+    <th width="20">URL</th>
+    <th width="20">Login</th>
+    <th width="20">Password</th>
+    <th width="20">Actions</th>
+  </tr>
+  </thead>
+  <tbody>
+<?php foreach($model as $n=>$repository): ?>
+  <tr class="<?php echo $n%2?'even':'odd';?>">
+    <td width="20"><?php echo CHtml::encode($repository->name); ?></td>
+    <td width="20"><?php echo CHtml::encode($repository->url); ?></td>
+    <td width="20"><?php echo CHtml::encode($repository->login); ?></td>
+    <td width="20"><?php echo CHtml::encode($repository->password); ?></td>
+    <td width="20">
+      <?php echo CHtml::link('Update',array('update','id'=>$repository->id)); ?>
+      <?php echo CHtml::linkButton('Delete',array(
+      	  'submit'=>'',
+      	  'params'=>array('command'=>'delete','id'=>$repository->id),
+      	  'confirm'=>"Are you sure to delete #{$repository->id}?")); ?>
+	</td>
+  </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
