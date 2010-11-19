b6f864ff10942fc26d6a1a59345b8ed0282c869d
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-19 20:33:54 +0100

    Debug mode off in production mode - should make it really fast.

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -17,7 +17,7 @@ class CommentController extends Controller {
      */
     public function filters() {
         return array(
-            'accessControl', // perform access control for CRUD operations
+            'rights', // perform access control for CRUD operations
         );
     }
 
@@ -26,24 +26,9 @@ class CommentController extends Controller {
      * This method is used by the 'accessControl' filter.
      * @return array access control rules
      */
-    public function accessRules() {
-        return array(
-            array('allow', // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view', 'feed'),
-                'users' => array('*'),
-            ),
-            array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('create', 'update'),
-                'users' => array('@'),
-            ),
-            array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => array('admin', 'delete'),
-                'users' => array('admin'),
-            ),
-            array('deny', // deny all users
-                'users' => array('*'),
-            ),
-        );
+    public function allowedActions()
+    {
+            return 'index, view';
     }
 
     /**
