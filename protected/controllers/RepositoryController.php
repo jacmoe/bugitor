6e2e5ea5a5fcbcd25271a4f46c69b3d9f3968240
Jacob Moen <jacmoe.dk@gmail.com>
2010-11-27 11:55:56 +0100

    sdf

--- protected/controllers/RepositoryController.php
+++ protected/controllers/RepositoryController.php
@@ -153,8 +153,8 @@ class RepositoryController extends Controller
                         }
 
 			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-                            $this->redirect(array('project/settings','identifier'=>$_GET['identifier'], 'tab' => 'repositories'));
+			//if(!isset($_GET['ajax']))
+                        $this->redirect(array('project/settings','identifier'=>$_GET['identifier'], 'tab' => 'repositories'));
 		}
 		else
 			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
