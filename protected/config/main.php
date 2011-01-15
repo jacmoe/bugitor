25b41f48c87984d283b37f07013f7570c2dabc6b
Jacob Moen <jacmoe.dk@gmail.com>
2011-01-15 22:44:41 +0100

    Small fixes for feed. Validates OK. :)

--- protected/config/main.php
+++ protected/config/main.php
@@ -160,7 +160,7 @@ return array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                'projects/<identifier>/feed'=>array('projects/feed', 'urlSuffix'=>'.xml', 'caseSensitive'=>false),
+                'projects/<identifier>/feed'=>array('project/feed', 'urlSuffix'=>'.xml', 'caseSensitive'=>false),
                 '/issues/' => 'issue/index',
                 ),
         ),
