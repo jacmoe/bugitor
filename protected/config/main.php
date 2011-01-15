b72f14c4ed7e5658a085e7ed92dad50e8514142f
jacmoe <jacmoe@localhost>
2011-01-15 22:37:07 +0100

    Project activity feed - take one

--- protected/config/main.php
+++ protected/config/main.php
@@ -160,7 +160,7 @@ return array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                'comment/feed'=>array('comment/feed', 'urlSuffix'=>'.xml', 'caseSensitive'=>false),
+                'projects/<identifier>/feed'=>array('projects/feed', 'urlSuffix'=>'.xml', 'caseSensitive'=>false),
                 '/issues/' => 'issue/index',
                 ),
         ),
