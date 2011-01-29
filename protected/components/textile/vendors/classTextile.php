b09a57e76219509890b31308df7d866317805f1f
Jacob Moen <jacmoe.dk@gmail.com>
2011-01-29 04:36:53 +0100

    Textilizer : Fixed style for code tags - max-height instead of height..

--- protected/components/textile/vendors/classTextile.php
+++ protected/components/textile/vendors/classTextile.php
@@ -845,9 +845,9 @@ class Textile
       @list(, $before, $text, $after) = $m;
       if ($this->restricted)
           // $text is already escaped
-            return $before.$this->shelve('<div class="scroll_small"><code>'.$text.'</code></div>').$after;
+            return $before.$this->shelve('<div class="scroll_code"><code>'.$text.'</code></div>').$after;
       else
-            return $before.$this->shelve('<div class="scroll_small"><code>'.$this->encode_html($text).'</code></div>').$after;
+            return $before.$this->shelve('<div class="scroll_code"><code>'.$this->encode_html($text).'</code></div>').$after;
     }
 
 // -------------------------------------------------------------
