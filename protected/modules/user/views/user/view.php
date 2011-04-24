713cfdb9c393fb5d72e00b81a73e5522d9869a47
jacmoe2 <jacmoe2@localhost>
2011-04-24 03:16:02 +0200

    User profile view page clean up

--- protected/modules/user/views/user/view.php
+++ protected/modules/user/views/user/view.php
@@ -1,46 +1,37 @@
-<?php
-$this->breadcrumbs=array(
-	UserModule::t('Users')=>array('index'),
-	$model->username,
-);
-?>
-<h1><?php echo UserModule::t('View User').' "'.$model->username.'"'; ?></h1>
-
+<div class="contextual_left">
+<?php echo Bugitor::gravatar($model); ?>
+</div>
+<h2><?php echo ucfirst($model->username); ?></h2>
+<br/>
+<div class="splitcontentleft">
+    <ul>
+        <li>Registered on: <?php echo date("d.m.Y H:i:s",$model->createtime); ?></li>
+        <li>Last connection: <?php echo (($model->lastvisit)?date("d.m.Y H:i:s",$model->lastvisit):UserModule::t('Never')) ?></li>
+    </ul>
+    <br/>
+<!--    <h3>Projects</h3>
+    <ul>
+        <li><a href="/projects/redmine">Redmine</a>
+        (Contributor, 2011-01-01)</li>
+    </ul>-->
+</div>
+<div class="splitcontentright">
+<!--<h3><a href="/activity?from=2011-04-23&amp;user_id=11192">Activity</a></h3>
+<p>
+Reported issues: 16
+</p>
+<div id="activity">
+<h4>2011-04-23</h4>
+<dl>
+  <dt class="changeset">
+  <span class="time">11:25</span>
+  <span class="project">Redmine</span>
+  <a href="/projects/redmine/repository/revisions/5543">Revision 5543: scm: mercurial: update locales for repository note in setting.</a></dt>
+  <dd><span class="description"></span></dd>
+</dl>
+</div>-->
+</div>
+<hr/>
 <ul class="actions">
-	<li><?php echo CHtml::link(UserModule::t('List User'),array('index')); ?></li>
+	<li><?php echo CHtml::link(UserModule::t('List Users'),array('index')); ?></li>
 </ul><!-- actions -->
-
-<?php 
-
-// For all users
-	$attributes = array(
-			'username',
-	);
-	
-	$profileFields=ProfileField::model()->forAll()->sort()->findAll();
-	if ($profileFields) {
-		foreach($profileFields as $field) {
-			array_push($attributes,array(
-					'label' => UserModule::t($field->title),
-					'name' => $field->varname,
-					'value' => $model->profile->getAttribute($field->varname),
-				));
-		}
-	}
-	array_push($attributes,
-		array(
-			'name' => 'createtime',
-			'value' => date("d.m.Y H:i:s",$model->createtime),
-		),
-		array(
-			'name' => 'lastvisit',
-			'value' => (($model->lastvisit)?date("d.m.Y H:i:s",$model->lastvisit):UserModule::t('Not visited')),
-		)
-	);
-			
-	$this->widget('zii.widgets.CDetailView', array(
-		'data'=>$model,
-		'attributes'=>$attributes,
-	));
-
-?>
