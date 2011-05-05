1b0c31dbef20a45bfd695ec652818109e1748db4
jacmoe2 <jacmoe2@localhost>
2011-05-06 00:57:45 +0200

    db.in.php template file added

---
+++ protected/config/db.in.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+            'connectionString' => 'mysql:host={host_in};dbname={dbname_in}',
+            'emulatePrepare' => true,
+            'username' => '{username_in}',
+            'tablePrefix' => '{tablePrefix_in}',
+            'password' => '{password_in}',
+            'charset' => '{charset_in}',
+        );
+
