723f4f7039ca6c178fb7cb6f60537d617e94fdbb
Jacob Moen <jacmoe.dk@gmail.com>
2011-05-15 10:22:54 +0200

    Directory permissions checked before installer continues. Pretty error/success messages.

--- installer/index.php
+++ installer/index.php
@@ -5,10 +5,12 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 if(!file_exists($yii))
 {
-    die("<font color=\"red\">Error:</font> Cannot find Yii!<br/>Please check and correct the path to Yii in <em>{$_SERVER['SERVER_NAME']}/installer/index.php</em> and <em>{$_SERVER['SERVER_NAME']}/index.php</em>");
+    die("<font color=\"red\">Error:</font> Cannot find Yii!<br/>
+            Please check and correct the path to Yii in <em>{$_SERVER['SERVER_NAME']}/installer/index.php</em> and <em>{$_SERVER['SERVER_NAME']}/index.php</em>");
 }
 if(file_exists(dirname(__FILE__).'/lock')) {
-    die("<font color=\"red\">Attention:</font> Installer has been locked!<br/>Please remove the lock file in the installer directory and try again.");
+    die("<font color=\"red\">Attention:</font> Installer has been locked.<br/>
+        Please remove the lock file in the installer directory and try again.");
 }
 require_once($yii);
 Yii::createWebApplication($config)->run();
--- installer/protected/views/install/systemcheck.php
+++ installer/protected/views/install/systemcheck.php
@@ -2,12 +2,13 @@
 echo $event->sender->menu->run();
 echo '<div>Step '.$event->sender->currentStep.' of '.$event->sender->stepCount;
 echo '<h3>'.$event->sender->getStepLabel($event->step).'</h3>';
-echo 'This is system check<br/>';
+echo 'Checking permissions..<br/><br/>';
 echo $message;
 if($failed) {
 Yii::app()->clientScript->registerScript('disableInstallButton',<<<EOD
 $("#installButton").attr('disabled', 'disabled');
 EOD
 ,CClientScript::POS_READY);
+echo '<br/>The installation cannot continue..<br/><br/>';
 }
 echo CHtml::tag('div',array('class'=>'form'),$form);
