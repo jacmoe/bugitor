344c664a3f9b3d6dfba12b27342fc05b40ccc417
jacmoe2 <jacmoe2@localhost>
2011-05-18 01:22:35 +0200

    Is this working?

--- protected/commands/HandleRepositoriesCommand.php
+++ protected/commands/HandleRepositoriesCommand.php
@@ -309,8 +309,6 @@ private function run_tool($toolname, $mode, $args = null)
                         $hg_executable = Yii::app()->config->get('hg_executable');
                         $cmd = "{$hg_executable} diff --git -r{$changeset->short_rev} -R {$this->repopath} --cwd {$this->repopath} {$change->path}";
                         $diff = stream_get_contents(popen($cmd, 'r'));
-//                        $fp = $this->run_tool('hg', 'read', array('diff', '-r' . $changeset->short_rev, '-R', $this->repopath, '--cwd', $this->repopath, '--git', $change->path));
-//                        $diff = fgets($fp);
                         echo $diff;
                         $change->diff = htmlspecialchars($diff);
                         $fp = null;
