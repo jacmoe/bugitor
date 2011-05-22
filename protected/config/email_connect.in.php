462baf847a06804ca5d858026b17feabc7c2397e
Jacob Moen <jacmoe.dk@gmail.com>
2011-05-22 08:44:58 +0200

    FetchEmailCommand now connects to an imap server and fetches unread messages and processes them. Encoding issues solved! :D Refs #614

---
+++ protected/config/email_connect.in.php
@@ -0,0 +1,6 @@
+<?php
+        // incoming email settings - for app/commands/FetchEmailCommand.php
+        $mailserver = "{mailserver_in}";
+        $port = "{port_in}";
+        $user = "{user_in}";
+        $pass = "{pass_in}";
