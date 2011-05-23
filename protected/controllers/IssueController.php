4594eda6a7821f1580d27630b70a660e77d6aa01
jacmoe2 <jacmoe2@localhost>
2011-05-23 15:52:05 +0200

    Notification model added. Now almost everything uses Issue.addNotification instead of Issue.sendNotification. References #719 and #715.

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -343,7 +343,7 @@ class IssueController extends Controller {
                     $model->save(false);
 
 
-                    $model->sendNotifications($model->id, $comment, $model->updated_by);
+                    $model->addNotifications($model->id, $comment->id, $model->updated_by);
 
                     if($has_details) {
                         if($model->status == 'swIssue/resolved') {
@@ -424,7 +424,7 @@ class IssueController extends Controller {
                     $model->save(false);
 
                     
-                    $model->sendNotifications($model->id, $comment, $model->updated_by);
+                    $model->addNotifications($model->id, $comment->id, $model->updated_by);
 
                     if($has_details) {
                         if($model->status == 'swIssue/resolved') {
---
+++ protected/migrations/m110523_133517_notification_table.php
@@ -0,0 +1,55 @@
+<?php
+
+class m110523_133517_notification_table extends CDbMigration
+{
+    public function up()
+    {
+        $this->createTable('{{notification}}',
+                array(
+                    "id" => "pk",
+                    "issue_id" => "integer NOT NULL",
+                    "comment_id" => "integer NOT NULL",
+                    "updated_by" => "integer NOT NULL",
+                ),"ENGINE=InnoDB DEFAULT CHARSET=utf8");
+        
+        $this->createIndex("fk_notification_issue_id", "{{notification}}", "issue_id");
+        $this->createIndex("fk_notification_comment_id", "{{notification}}", "comment_id");
+        $this->createIndex("fk_notification_updated_by", "{{notification}}", "updated_by");
+        
+        $this->addForeignKey('fk_notification_issue_id',
+            '{{notification}}',
+            'issue_id',
+            '{{issue}}', 'id',
+            'CASCADE', 'CASCADE'
+        );
+        $this->addForeignKey('fk_notification_comment_id',
+            '{{notification}}',
+            'comment_id',
+            '{{comment}}', 'id',
+            'CASCADE', 'CASCADE'
+        );
+        $this->addForeignKey('fk_notification_updated_by',
+            '{{notification}}',
+            'updated_by',
+            '{{users}}', 'id',
+            'CASCADE', 'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+            echo "m110523_133517_notification_table does not support migration down.\n";
+            return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
