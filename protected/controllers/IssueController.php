adc4d94071a8e95b033f4656fecc31e81c075b96
Jacob Moen <jacmoe.dk@gmail.com>
2011-05-23 16:20:08 +0200

    Oops - looks like NetBean's refactor screwed up. It's 'addNotification', not 'addNotifications'..

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -343,7 +343,7 @@ class IssueController extends Controller {
                     $model->save(false);
 
 
-                    $model->addNotifications($model->id, $comment->id, $model->updated_by);
+                    $model->addNotification($model->id, $comment->id, $model->updated_by);
 
                     if($has_details) {
                         if($model->status == 'swIssue/resolved') {
@@ -424,7 +424,7 @@ class IssueController extends Controller {
                     $model->save(false);
 
                     
-                    $model->addNotifications($model->id, $comment->id, $model->updated_by);
+                    $model->addNotification($model->id, $comment->id, $model->updated_by);
 
                     if($has_details) {
                         if($model->status == 'swIssue/resolved') {
