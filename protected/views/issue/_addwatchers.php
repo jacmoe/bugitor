c753ab1f208eda173d22fa1281335f498288c41c
Jacob Moen <jacmoe.dk@gmail.com>
2011-09-20 17:10:05 +0200

    Closing issue, although the add/remove watcher still needs some improvements.. For some reason, the alert for issue delete is being triggered by the remove watcher button, so removing the 'delete issue' link. Deleting issues can be done from the issue index. And it still doesn't refresh properly upon removal of watcher. But it works. Closes #505

--- protected/views/issue/_addwatchers.php
+++ protected/views/issue/_addwatchers.php
@@ -3,7 +3,7 @@ $nonwatcherslist = $model->nonWatchersList;
 if (null != $nonwatcherslist) {
 echo 'Add watcher:'.PHP_EOL;
 echo CHTML::beginForm('#').PHP_EOL;
-echo CHTML::dropDownList('add_watcher', '', $nonwatcherslist).PHP_EOL; //$model->getNonWatchersList();
+echo CHTML::dropDownList('add_watcher', '', $nonwatcherslist).PHP_EOL;
 echo CHTML::ajaxSubmitButton('Add',$this->createUrl('addwatcher', array('issue_id' => $model->id)),
         array('update' => '#watchers',
             'type' => "post")
@@ -12,8 +12,9 @@ echo CHTML::endForm().PHP_EOL;
 }
 $watcherslist = $model->watchersList;
 if (null != $watcherslist) {
+echo 'Remove watcher:'.PHP_EOL;
 echo CHTML::beginForm('#').PHP_EOL;
-echo CHTML::dropDownList('remove_watcher', '', $watcherslist).PHP_EOL; //$model->getNonWatchersList();
+echo CHTML::dropDownList('remove_watcher', '', $watcherslist).PHP_EOL;
 echo CHTML::ajaxSubmitButton('Remove',$this->createUrl('removewatcher', array('issue_id' => $model->id)),
         array('update' => '#watchers',
             'type' => "post")
