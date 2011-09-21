3ee68f2099425ddc2f404c4ce061f83e3a0986cd
jacmoe <jacmoe@mail.dk>
2011-09-21 18:28:03 +0200

    Case sensitivity fix - CHtml instead of CHTML - closes #802

--- protected/views/issue/_addwatchers.php
+++ protected/views/issue/_addwatchers.php
@@ -2,13 +2,12 @@
 $nonwatcherslist = $model->nonWatchersList;
 if (null != $nonwatcherslist) {
 echo 'Add watcher:'.PHP_EOL;
-echo CHTML::beginForm('#').PHP_EOL;
-//echo CHTML::dropDownList('add_watcher', '', $nonwatcherslist).PHP_EOL;
+echo CHtml::beginForm('#').PHP_EOL;
 $this->widget('ext.combobox.EJuiComboBox', array(
     'name' => 'add_watcher',
     'data' => $nonwatcherslist,
 ));
-echo CHTML::ajaxSubmitButton('Add',$this->createUrl('addwatcher', array('issue_id' => $model->id)),
+echo CHtml::ajaxSubmitButton('Add',$this->createUrl('addwatcher', array('issue_id' => $model->id)),
         array('update' => '#watchers',
             'type' => "post",
             'complete' => 'function(data,status){
@@ -16,21 +15,20 @@ echo CHTML::ajaxSubmitButton('Add',$this->createUrl('addwatcher', array('issue_i
            }'
 ), array('id' => 'add-watcher-button')
         ).PHP_EOL;
-echo CHTML::endForm().PHP_EOL;
+echo CHtml::endForm().PHP_EOL;
 }
 $watcherslist = $model->watchersList;
 if (null != $watcherslist) {
 echo 'Remove watcher:'.PHP_EOL;
-echo CHTML::beginForm('#').PHP_EOL;
-//echo CHTML::dropDownList('remove_watcher', '', $watcherslist).PHP_EOL;
+echo CHtml::beginForm('#').PHP_EOL;
 $this->widget('ext.combobox.EJuiComboBox', array(
     'name' => 'remove_watcher',
     'data' => $watcherslist,
 ));
-echo CHTML::ajaxSubmitButton('Remove',$this->createUrl('removewatcher', array('issue_id' => $model->id)),
+echo CHtml::ajaxSubmitButton('Remove',$this->createUrl('removewatcher', array('issue_id' => $model->id)),
         array('update' => '#watchers',
             'type' => "post"), array('id' => 'remove-watcher-button')
         ).PHP_EOL;
-echo CHTML::endForm().PHP_EOL;
+echo CHtml::endForm().PHP_EOL;
 }
 ?>
