9c9a87a467c58fa313f2d108cc38ed0a504b48c7
jacmoe <jacmoe@localhost>
2011-10-31 13:55:09 +0100

    Sprites, images. New design/elements folder.

--- themes/new/views/issue/create.php
+++ themes/new/views/issue/create.php
@@ -34,5 +34,5 @@
 <?php
 $this->pageTitle = $project_name . 'New Issue - ' . Yii::app()->name ;
 ?>
-<h3 class="issues">New Issue</h3>
+<h3 class="issues-icon">New Issue</h3>
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
