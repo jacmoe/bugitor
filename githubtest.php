45ad7f653df801303ac5b4db663694788c8a0422
Jacob Moen <jacmoe.dk@gmail.com>
2011-12-01 23:15:59 +0100

    Github and SVN can now retrieve and render unified diffs. Ironically, the Bitbucket API doesn't seem to support diffs..

--- githubtest.php
+++ githubtest.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+	<!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="/assets/f105b38b/css/chaw.css" />
+<link rel="stylesheet" type="text/css" href="/assets/cad80fdb/styles/github.css" />
+<script type="text/javascript" src="/assets/a87bfd91/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/f105b38b/jquery.highlight_diff.min.js"></script>
+<title>Github API test</title>
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<link rel="stylesheet" type="text/css" href="/themes/new/css/screen.css" />
+		<!-- Favicons
+		================================================== -->
+		<link rel="shortcut icon" href="/themes/new/images/favicon.ico">
+		<link rel="apple-touch-icon" href="/themes/new/images/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/themes/new/images/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/themes/new/images/apple-touch-icon-114x114.png">
+        
+        <script type="text/javascript" src="/assets/cad80fdb/highlight.pack.js"></script>
+        <script type="text/javascript">
+        /*<![CDATA[*/
+        hljs.initHighlightingOnLoad();
+        /*]]>*/
+        </script>
+	</head>
+	<body>
 <?php
 require_once 'protected/vendors/php-github-api/lib/Github/Autoloader.php';
 Github_Autoloader::register();
@@ -14,10 +49,20 @@ echo "<pre>";
 print_r($repo);
 echo "</pre>";*/
 
-/*$commit = $github->getCommitApi()->getCommit('jacmoe', 'highlighter', 'db7d468ee1920a8e0bbe27093797950743faa6cc');
-echo "<pre>";
+$commit = $github->getCommitApi()->getCommit('jacmoe', 'highlighter', '0a0c5b493f44bd45f7e8320f5676a9595becae9c');
+/*echo "<pre>";
 print_r($commit);
 echo "</pre>";*/
+
+foreach($commit['modified'] as $modified){
+    echo "<div class=diff>";
+    echo "diff --git\n";
+    //echo "<pre>";
+    echo htmlspecialchars($modified['diff']);
+    //echo "</pre>";
+    echo "</div>";
+    echo "<hr/>";
+}
 
 foreach($commits as $commit)
 {
@@ -55,3 +100,13 @@ foreach($commits as $commit)
     echo "<hr/>";
     echo "<br/>";
 }
+?>
+	<script type="text/javascript">
+/*<![CDATA[*/
+jQuery(function($) {
+$(".diff").highlight_diff();
+});
+/*]]>*/
+</script>
+</body>
+</html>
--- svntest.php
+++ svntest.php
@@ -1,6 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+	<!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="/assets/f105b38b/css/chaw.css" />
+<link rel="stylesheet" type="text/css" href="/assets/cad80fdb/styles/github.css" />
+<script type="text/javascript" src="/assets/a87bfd91/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/f105b38b/jquery.highlight_diff.min.js"></script>
+<title>phpsvnclient SVN test</title>
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<link rel="stylesheet" type="text/css" href="/themes/new/css/screen.css" />
+		<!-- Favicons
+		================================================== -->
+		<link rel="shortcut icon" href="/themes/new/images/favicon.ico">
+		<link rel="apple-touch-icon" href="/themes/new/images/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/themes/new/images/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/themes/new/images/apple-touch-icon-114x114.png">
+	</head>
+	<body>
 <?php
 $url = 'http://phpsvnclient.googlecode.com/svn/';
 
+$path = 'protected/vendors/phpsvnclient';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+$path = 'protected/vendors/phpsvnclient/ext';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+echo get_include_path() . "<br/><hr/>";
 require_once('protected/vendors/phpsvnclient/phpsvnclient.php');
 $phpsvnclient = new phpsvnclient($url);
 
@@ -9,8 +42,25 @@ echo "Version: " . $version . "<br/>";
 
 // Lets get an array of the contents within /trunk/
 //$logs = $phpsvnclient->getRepositoryLogs('/trunk/');
-$logs = $phpsvnclient->getRepositoryLogs();
+/*$logs = $phpsvnclient->getRepositoryLogs();
 
 echo "<pre>\n";
 print_r($logs);
-echo "</pre>\n";
+echo "</pre>\n";*/
+
+$diff = $phpsvnclient->diffVersions("trunk/phpsvnclient.php", 113, 131);
+echo "<div class=diff>\n";
+echo $diff;
+echo "</div>\n";
+
+
+?>
+	<script type="text/javascript">
+/*<![CDATA[*/
+jQuery(function($) {
+$(".diff").highlight_diff();
+});
+/*]]>*/
+</script>
+</body>
+</html>
