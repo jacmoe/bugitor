e41b928d21060f652471e8eb329db23fafe3b87f
Jacob Moen <jacmoe.dk@gmail.com>
2011-12-04 14:49:01 +0100

    Github works - without auto-loader. Refs #625

--- protected/components/scm/GithubSCMBackend.php
+++ protected/components/scm/GithubSCMBackend.php
@@ -1,4 +1,39 @@
 <?php
+Yii::import('application.vendors.php-github-api.lib.Github.*');
+//require_once()'Autoloader.php');
+require_once('Client.php');
+require_once('HttpClientInterface.php');
+require_once('HttpClient.php');
+require_once('HttpClient/Curl.php');
+require_once('ApiInterface.php');
+require_once('Api.php');
+require_once('Api/Commit.php');
+
+
 class GithubSCMBackend extends SCMBackend
 {
+    private $_github = null;
+
+    public $name = 'github';
+
+    /*public function __construct()
+    {
+        //Github_Autoloader::register();
+    }*/
+
+    private function getGithub()
+    {
+        if(null !== $this->_github){
+            return $this->_github;
+        } else {
+            $this->_github = new Github_Client();
+            return $this->_github;
+        }
+    }
+
+    public function log($start = 0, $end = '', $limit = 100)
+    {
+        $commits = $this->getGithub()->getCommitApi()->getBranchCommits('jacmoe', 'highlighter', 'master');
+        return $commits[0];
+    }
 }
