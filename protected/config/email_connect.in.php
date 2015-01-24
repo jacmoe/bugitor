8ef46bb96abc8c9dd4b77a2fdf0bc1ad72c613f3
jacmoe <jacmoe@localhost>
2015-01-24 08:59:56 +0100

    Removing old stuff - part 2

--- protected/config/email_connect.in.php
+++
@@ -1,6 +0,0 @@
-<?php
-        // incoming email settings - for app/commands/FetchEmailCommand.php
-        $mailserver = "{mailserver_in}";
-        $port = "{port_in}";
-        $user = "{user_in}";
-        $pass = "{pass_in}";
--- protected/config/mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'class' => 'ext.yii-mail.YiiMail',
-    'transportType' => 'php',
-    'viewPath' => 'application.views.mail',
-    'dryRun' => false,
-);
