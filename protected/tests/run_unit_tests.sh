db13efdcd6a812ea8db4d35956465b0c6a96b33f
Jacob Moen <jacmoe.dk@gmail.com>
2015-01-24 09:05:47 +0100

    Removing old stuff - part 4

--- protected/tests/run_unit_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-phpunit --debug unit/DbTest.php
-phpunit --debug unit/ProjectTest.php
-phpunit --debug unit/UserTest.php
--- protected/tests/unit/DbTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DbTest extends CTestCase
-{
-	public function testDbConnection() {
-		print "Testing Db connection...";
-		$this->assertNotEquals(NULL, Yii::app()->db);	
-	}
-}
