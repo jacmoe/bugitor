6a59596421dad2cbc6364e45f08eae73204079b8
Jacob Moen <jacmoe.dk@gmail.com>
2015-02-24 14:54:48 +0100

    Set up twig renderer and url manager

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,32 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ],
+        ],
+        'view' => [
+            'class' => 'yii\web\View',
+            'renderers' => [
+                'html' => [
+                    'class' => 'yii\twig\ViewRenderer',
+                    'cachePath' => '@runtime/Twig/cache',
+                    'options' => [
+                    'auto_reload' => true,
+                    ],
+                    'globals' => [
+                        'html' => '\yii\helpers\html',
+                    ],
+                    'uses' => [
+                        'yii\bootstrap',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
