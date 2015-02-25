6505ebcd4842299547fa37715c41ad14ffe00947
jacmoe <jacmoe.dk@gmail.com>
2015-02-25 22:33:52 +0100

    Theming continued

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,6 +12,11 @@ return [
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+        'assetManager' => [
+            'bundles' => [
+                'yii\bootstrap\BootstrapAsset' => false,
+            ],
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
