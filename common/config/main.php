cec484bf8060ddc347bcdd7e0686c7727795a36f
Jacob Moen <jacmoe.dk@gmail.com>
2015-02-26 17:52:37 +0100

    Setting up Kartik and Gii

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,31 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'gridview' => [
+            'class' => '\kartik\grid\Module',
+        ],
+        'dynagrid' => [
+            'class' => '\kartik\dynagrid\Module',
+        ],
+        'datecontrol' =>  [
+            'class' => 'kartik\datecontrol\Module',
+            // format settings for displaying each date attribute
+            'displaySettings' => [
+                'date' => 'd-m-Y',
+                'time' => 'H:i:s A',
+                'datetime' => 'd-m-Y H:i:s A',
+            ],
+            // format settings for saving each date attribute
+            'saveSettings' => [
+                'date' => 'Y-m-d', 
+                'time' => 'H:i:s',
+                'datetime' => 'Y-m-d H:i:s',
+            ],
+            // automatically use kartik\widgets for each of the above formats
+            'autoWidget' => true,
+        ]
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -21,7 +46,7 @@ return [
                     'class' => 'yii\twig\ViewRenderer',
                     'cachePath' => '@runtime/Twig/cache',
                     'options' => [
-                    'auto_reload' => true,
+                        'auto_reload' => true,
                     ],
                     'globals' => [
                         'html' => '\yii\helpers\html',
