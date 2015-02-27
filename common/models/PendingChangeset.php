23deb119c0c733ad0cd7568e62e96c867108b467
jacmoe <jacmoe.dk@gmail.com>
2015-02-27 22:00:27 +0100

    Models

---
+++ common/models/PendingChangeset.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%pending_changeset}}".
+ *
+ * @property integer $id
+ * @property integer $changeset_id
+ *
+ * @property Changeset $changeset
+ */
+class PendingChangeset extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%pending_changeset}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['changeset_id'], 'required'],
+            [['changeset_id'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'changeset_id' => Yii::t('app', 'Changeset ID'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChangeset()
+    {
+        return $this->hasOne(Changeset::className(), ['id' => 'changeset_id']);
+    }
+}
---
+++ common/models/Version.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%version}}".
+ *
+ * @property integer $id
+ * @property integer $project_id
+ * @property string $name
+ * @property string $description
+ * @property string $effective_date
+ * @property string $created
+ * @property string $modified
+ * @property integer $version_order
+ * @property string $title
+ *
+ * @property Issue[] $issues
+ * @property Project $project
+ */
+class Version extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%version}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['project_id', 'version_order'], 'integer'],
+            [['name'], 'required'],
+            [['description'], 'string'],
+            [['effective_date', 'created', 'modified'], 'safe'],
+            [['name', 'title'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'project_id' => Yii::t('app', 'Project ID'),
+            'name' => Yii::t('app', 'Name'),
+            'description' => Yii::t('app', 'Description'),
+            'effective_date' => Yii::t('app', 'Effective Date'),
+            'created' => Yii::t('app', 'Created'),
+            'modified' => Yii::t('app', 'Modified'),
+            'version_order' => Yii::t('app', 'Version Order'),
+            'title' => Yii::t('app', 'Title'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIssues()
+    {
+        return $this->hasMany(Issue::className(), ['version_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProject()
+    {
+        return $this->hasOne(Project::className(), ['id' => 'project_id']);
+    }
+}
