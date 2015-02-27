70fa2c823832b906bfae6cea38ef9f676ebf88e0
Jacob Moen <jacmoe.dk@gmail.com>
2015-02-27 18:12:16 +0100

    sdf

--- tests/codeception/frontend/acceptance/HomeCept.php
+++ tests/codeception/frontend/acceptance/HomeCept.php
@@ -4,7 +4,7 @@ use tests\codeception\frontend\AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
+$I->see('Bugitor');
 $I->seeLink('About');
 $I->click('About');
 $I->see('This is the About page.');
