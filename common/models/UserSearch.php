aac47326561c66c4a9c5531488a47ffeb8cabe70
jacmoe <jacmoe.dk@gmail.com>
2015-03-03 16:07:23 +0100

    User crud

---
+++ common/models/UserSearch.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\User;
+
+/**
+ * UserSearch represents the model behind the search form about User.
+ */
+class UserSearch extends Model
+{
+	public $id;
+	public $username;
+	public $auth_key;
+	public $password_hash;
+	public $password_reset_token;
+	public $email;
+	public $status;
+	public $created_at;
+	public $updated_at;
+
+	public function rules()
+	{
+		return [
+			[['id', 'status', 'created_at', 'updated_at'], 'integer'],
+			[['username', 'auth_key', 'password_hash', 'password_reset_token', 'email'], 'safe'],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'username' => 'Username',
+			'auth_key' => 'Auth Key',
+			'password_hash' => 'Password Hash',
+			'password_reset_token' => 'Password Reset Token',
+			'email' => 'Email',
+			'status' => 'Status',
+			'created_at' => 'Created At',
+			'updated_at' => 'Updated At',
+		];
+	}
+
+	public function search($params)
+	{
+		$query = User::find();
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
+
+		if (!($this->load($params) && $this->validate())) {
+			return $dataProvider;
+		}
+
+		$query->andFilterWhere([
+            'id' => $this->id,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+		$query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'auth_key', $this->auth_key])
+            ->andFilterWhere(['like', 'password_hash', $this->password_hash])
+            ->andFilterWhere(['like', 'password_reset_token', $this->password_reset_token])
+            ->andFilterWhere(['like', 'email', $this->email]);
+
+		return $dataProvider;
+	}
+
+	protected function addCondition($query, $attribute, $partialMatch = false)
+	{
+		$value = $this->$attribute;
+		if (trim($value) === '') {
+			return;
+		}
+		if ($partialMatch) {
+			$value = '%' . strtr($value, ['%'=>'\%', '_'=>'\_', '\\'=>'\\\\']) . '%';
+			$query->andWhere(['like', $attribute, $value]);
+		} else {
+			$query->andWhere([$attribute => $value]);
+		}
+	}
+}
