ff432a3025c59ffe960c8fd963da705cf79459d7
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-07 13:08:53 +0100

    Re-jading

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
   'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-  'layout' => 'main.php',
+  'layout' => 'main.jade',
   'components' => [
     'cache' => [
       'class' => 'yii\caching\FileCache',
