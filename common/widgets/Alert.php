e43e14b245c60895a64bc82b78d90f4272785c24
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-07 13:11:24 +0100

    Alert widget moved

--- common/widgets/Alert.php
+++ common/widgets/Alert.php
@@ -5,22 +5,22 @@
  * @license http://www.yiiframework.com/license/
  */
 
-namespace frontend\widgets;
+namespace common\widgets;
 
 /**
  * Alert widget renders a message from session flash. All flash messages are displayed
  * in the sequence they were assigned using setFlash. You can set message as following:
  *
  * ```php
- * \Yii::$app->getSession()->setFlash('error', 'This is the message');
- * \Yii::$app->getSession()->setFlash('success', 'This is the message');
- * \Yii::$app->getSession()->setFlash('info', 'This is the message');
+ * \Yii::$app->session->setFlash('error', 'This is the message');
+ * \Yii::$app->session->setFlash('success', 'This is the message');
+ * \Yii::$app->session->setFlash('info', 'This is the message');
  * ```
  *
  * Multiple messages could be set as follows:
  *
  * ```php
- * \Yii::$app->getSession()->setFlash('error', ['Error 1', 'Error 2']);
+ * \Yii::$app->session->setFlash('error', ['Error 1', 'Error 2']);
  * ```
  *
  * @author Kartik Visweswaran <kartikv2@gmail.com>
@@ -51,7 +51,7 @@ class Alert extends \yii\bootstrap\Widget
     {
         parent::init();
 
-        $session = \Yii::$app->getSession();
+        $session = \Yii::$app->session;
         $flashes = $session->getAllFlashes();
         $appendCss = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
 
