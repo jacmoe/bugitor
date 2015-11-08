7b383a803cd47df82f6d76bbc80cb8d8b3a7b527
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-08 01:32:23 +0100

    Rbac Admin module added to backend

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,10 +12,28 @@ return [
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+        'authManager' => [
+          'class' => 'yii\rbac\PhpManager',
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-        ],
+        ],// user
+        'assetManager' => [
+            'bundles' => [
+                //'yii\bootstrap\BootstrapAsset' => false,
+                //'yii\validators\ValidationAsset' => false,
+                //'yii\web\YiiAsset' => false,
+                //'yii\widgets\ActiveFormAsset' => false,
+                //'yii\bootstrap\BootstrapPluginAsset' => false,
+                'yii\web\JqueryAsset' => [
+                    'js' => [
+                        //YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                        'jquery.min.js',
+                    ]
+                ],
+            ],
+        ],// assetManager
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -24,10 +42,17 @@ return [
                     'levels' => ['error', 'warning'],
                 ],
             ],
-        ],
+        ],// log
         'errorHandler' => [
             'errorAction' => 'site/error',
-        ],
+        ],// errorHandler
+    ],// components
+    'as access' => [
+      'class' => 'mdm\admin\classes\AccessControl',
+      'allowActions' => [
+        'site/*',
+        'admin/*',
+      ],
     ],
     'params' => $params,
 ];
