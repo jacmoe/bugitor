4f79c1a237fd076b5b640c7c5a07c382a928c60c
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-10 23:17:48 +0100

    Gravatar, name and member since now uses current user details

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use common\models\User;
-use yii\data\ActiveDataProvider;
+use common\models\UserSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -32,11 +32,11 @@ class UserController extends Controller
      */
     public function actionIndex()
     {
-        $dataProvider = new ActiveDataProvider([
-            'query' => User::find(),
-        ]);
+        $searchModel = new UserSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
+            'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
         ]);
     }
