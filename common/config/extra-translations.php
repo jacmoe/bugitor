f4136d364a327160f062fbc6e9bb2fe123418129
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-13 19:03:56 +0100

    i18n - a pity that Yii does not extract translations from jade templates

---
+++ common/config/extra-translations.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Because Yii does not extract translations from jade templates
+ * due to the missing <?php tags.
+ */
+
+$Navigation = Yii::t('app','Navigation');
+$Projects = Yii::t('app','Projects');
+$Dashboard = Yii::t('app','Dashboard');
+$Help = Yii::t('app','Help');
