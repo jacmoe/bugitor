94dd1b84357f1c555028ed6e476ff5506f9ab39f
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-17 17:20:24 +0100

    Remove test code

--- frontend/controllers/HelpController.php
+++ frontend/controllers/HelpController.php
@@ -24,13 +24,6 @@ class HelpController extends \yii\web\Controller
 
     public function actionIndex()
     {
-        \Yii::$app->mailqueue->compose()
-            ->setFrom('root@example.tst')
-            ->setTo('jacmoe@example.tst')
-            ->setSubject('Message subject')
-            ->setTextBody('Plain text content')
-            ->setHtmlBody('<b>HTML content</b>')
-            ->queue();
         return $this->render('index');
     }
 
