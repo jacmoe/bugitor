ed100b39de312601939083a4556e38165c86c9b3
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-17 20:58:36 +0100

    Issue boilerplate

---
+++ frontend/components/DashboardWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace frontend\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class DashboardWidget extends Widget
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+    }
+}
