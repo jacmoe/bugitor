bf343106f9e9f7297ddfdd18f0952887f84458d9
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-30 13:08:00 +0100

    sdf

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -9,10 +9,6 @@ namespace frontend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
@@ -22,8 +18,5 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
         'js/all.js',
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
     ];
 }
