119d0ccbefc7a18db00b2cb83b8089afb28fbacf
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-25 13:36:48 +0100

    Adding unit test for common models project

---
+++ tests/codeception/common/unit/models/ProjectTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace tests\codeception\common\models;
+
+class ProjectTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+    }
+
+}
