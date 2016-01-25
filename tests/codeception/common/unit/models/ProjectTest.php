b7546bed6df722a69e5cc23748492fa376969789
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-25 13:44:05 +0100

    Codeception: I have no idea what I am doing ;)

--- tests/codeception/common/unit/models/ProjectTest.php
+++ tests/codeception/common/unit/models/ProjectTest.php
@@ -1,19 +1,37 @@
 <?php
-namespace tests\codeception\common\models;
+namespace tests\codeception\common\unit\models;
 
-class ProjectTest extends \PHPUnit_Framework_TestCase
+use Yii;
+use tests\codeception\common\unit\DbTestCase;
+use Codeception\Specify;
+//use common\models\LoginForm;
+use tests\codeception\common\fixtures\ProjectFixture;
+
+class ProjectTest extends DbTestCase
 {
+    use Specify;
+
     protected function setUp()
     {
+        parent::setUp();
     }
 
     protected function tearDown()
     {
+        parent::tearDown();
     }
 
-    // tests
-    public function testMe()
+    /**
+     * @inheritdoc
+     */
+    public function fixtures()
     {
+        return [
+            'project' => [
+                'class' => UserFixture::className(),
+                'dataFile' => '@tests/codeception/common/unit/fixtures/data/models/project.php'
+            ],
+        ];
     }
 
 }
