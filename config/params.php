b980f600bc5efddf9f835d7153b05d07cc514149
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-11 16:31:24 +0100

    Gravatar and Yii2-user

--- config/params.php
+++ config/params.php
@@ -2,4 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    "yii.migrations"=> [
+        "@dektrium/user/migrations",
+        //"@vendor/nterms/yii2-mailqueue/migrations",
+    ],
 ];
