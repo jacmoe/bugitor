0bb9df1c45b1728b095ce6038bce9904f44c12e5
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-14 17:56:33 +0100

    Cookie law script

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,11 @@
-
 $(document).ready(function() {
-  // do stuff
+    $.cookiesDirective({
+      backgroundOpacity: '85',
+      backgroundColor: '#1E6887',
+      linkColor: '#FFF',
+      position: 'bottom',
+      explicitConsent: false,
+      message: 'This website uses cookies for analytics and persistent logins. By visiting this site you accept the use of cookies.',
+      privacyPolicyUri: 'cookies'
+    });
 });
