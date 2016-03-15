0b683b96594aa7012e0bacbbc56a01965522d622
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-15 21:48:56 +0100

    Testing : learning how to crawl

--- tests/codeception/unit/fixtures/data/project.php
+++ tests/codeception/unit/fixtures/data/project.php
@@ -1,27 +1,27 @@
 <?php
 
 return [
-    [
-        'name' => 'Caesar Rice',
+    'project1' => [
+        'name' => 'First project',
         'description' => 'Sint quia quam odio sint non aperiam rerum ipsa harum ipsam consectetur soluta.',
         'homepage' => 'http://pagac.com/',
         'public' => true,
-        'created' => 1059765934,
-        'modified' => 974841320,
-        'identifier' => 'Mrs. Velda Torphy',
+        'created' => time(),
+        'modified' => time(),
+        'identifier' => 'first_project',
         'logo' => null,
         'logoname' => null,
         'owner_id' => 1,
         'updater_id' => 1,
     ],
-    [
-        'name' => 'Carter Pfannerstill',
+    'project2' => [
+        'name' => 'Second project',
         'description' => 'Ut itaque laudantium dolor aut dolorem dolorem eligendi repudiandae enim facere.',
         'homepage' => 'http://morar.com/voluptas-officia-cumque-consequatur-hic-earum-nesciunt-unde.html',
         'public' => true,
-        'created' => 244345706,
-        'modified' => 1406920111,
-        'identifier' => 'Dr. Zena Hodkiewicz II',
+        'created' => time(),
+        'modified' => time(),
+        'identifier' => 'second_project',
         'logo' => null,
         'logoname' => null,
         'owner_id' => 1,
---
+++ tests/codeception/unit/models/IssueTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace tests\codeception\unit\models;
+
+use yii\codeception\DbTestCase;
+use app\models\Issue;
+use tests\codeception\unit\fixtures\IssueFixture;
+
+use Codeception\Util\Debug;
+
+class IssueTest extends DbTestCase
+{
+    public function fixtures()
+    {
+        return [
+            'issue' => IssueFixture::className(),
+        ];
+    }
+
+    public function testCreate()
+    {
+        $issue = new Issue();
+        $issue->tracker_id = 1;
+        $issue->project_id = 1;
+        $issue->subject = 'Stuffisodfj';
+        $issue->description = 'description here sdafa sadf saf';
+        $issue->user_id = 1;
+        $issue->issue_priority_id = 1;
+        $issue->version_id = 1;
+        $issue->assigned_to = 1;
+        $issue->created = time();
+        $issue->modified = time();
+        $issue->done_ratio = 0;
+        $issue->status = 'ok';
+        $issue->closed = 0;
+        $issue->pre_done_ratio = 0;
+        $issue->updated_by = 1;
+        $issue->last_comment = null;
+        $this->assertTrue($issue->validate(), 'Issue should validate');
+        Debug::debug($issue->errors);
+        // issue can't be saved until the user model has been fixtured
+        //$this->assertTrue($issue->save(), 'Issue should save');
+    }
+
+}
