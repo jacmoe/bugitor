12d605100c3275329e81ad142b020419c0fdd533
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-19 00:24:27 +0100

    Testing : Selenium works - and AdminCept too - hooray!

--- tests/codeception/acceptance/AdminCept.php
+++ tests/codeception/acceptance/AdminCept.php
@@ -3,10 +3,24 @@ use bugitor\tests\AcceptanceTester\AdminSteps as AdminTester;
 
 $I = new AdminTester($scenario);
 $I->loginAsAdmin();
+if (method_exists($I, 'wait')) {
+    $I->wait(3); // only for selenium
+}
 $I->expectTo('see logout link');
 $I->see('Logout');
 $I->expectTo('see admin link');
 $I->see('User Admin');
-//$I->expectTo('be able to go to the user admin page');
-//$I->amOnPage('/user/admin');
-//$I->seeLink('Create');
+$I->expectTo('be able to go to the user admin page');
+$I->click('User Admin');
+if (method_exists($I, 'wait')) {
+    $I->wait(3); // only for selenium
+}
+$I->expectTo('be on the User admin page');
+$I->see('Manage users');
+
+$I->click('Logout');
+if (method_exists($I, 'wait')) {
+    $I->wait(3); // only for selenium
+}
+$I->expectTo('see login link');
+$I->see('Login');
